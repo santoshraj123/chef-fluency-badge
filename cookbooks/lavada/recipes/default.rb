2aac31f7be840c7ec53438ec8931d1e504fa21be
santoshraj123 <santoshraj@yahoo.com>
2017-03-28 18:06:53 +0000

    lavada added

---
+++ cookbooks/lavada/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: lavada
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "httpd" do 
+	action :install
+end
